39c739c263cdde11428484469ce385c4ee52a212
Gino Lomelino <ginolomelino@gmail.com>
2012-05-15 05:38:29 -0400

    FPS counter & JS inheritance to the rescue

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
 	<link rel="stylesheet" type="text/css" src="css/styles.css"></link>
 </head>
 <body>
+	<div id="fps">fps: 0</div>
 	<canvas id="playfield" width="680px" height="640px"></canvas>
 	<script type="text/javascript">
 		var game = new Game();
 		game.Run();
+
+		var fpsOut = document.getElementById('fps');
+		setInterval(function(){
+		  fpsOut.innerHTML = "FPS: " + fps.toFixed(1);
+		}, 1000);
 	</script>
 </body>
 </html>
--- js/game.js
+++ js/game.js
@@ -4,41 +4,39 @@ var canvas = undefined;
 var buffer = undefined;
 var fieldWidth = 0;
 var fieldHeight = 0;
+var measureFPS = true;
+var debug = true;
+
+// variables for fps measurements
+var fps = 0, now, lastUpdate = (new Date)*1 - 1;
+// Smooths out fps rate so minor hickups don't kill it
+var fpsFilter = 50;
 
 var directions = {up:{value:0},down:{value:1},left:{value:2},right:{value:3}};
-var commands = {start:{value:'start'},stop:{value:'stop'}};
 var snowLevels = {blizzard:{value:30,name:'blizzard'},high:{value:15,name:'high'},medium:{value:8,name:'medium'},low:{value:2,name:'low'}};
+var flakeSizes = {small:{value:0,name:'small'},medium:{value:1,name:'medium'},large:{value:2,name:'large'}};
+
+// Use requestAnimFrame and fallback to setTimeout if not supported
+window.requestAnimFrame = ( function() {
+	return  window.requestAnimationFrame || 
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.oRequestAnimationFrame || 
+	window.msRequestAnimationFrame ||
+	function( callback ){ window.setTimeout(callback, 1000 / 60); };
+} )();
+
 
 // parse input
-function inputHandler(event,paddle,ball) {
+function inputHandler(event) {
 	switch(event.keyCode) {
 		case 37: //left arrow
-			if (event.type == 'keydown') {
-				paddle.SetDirection(commands.start,directions.left);
-			} else {
-				paddle.SetDirection(commands.stop,directions.left);
-			}
 			break;
 		case 38: // up arrow
-			if (event.type == 'keydown') {
-				paddle.SetDirection(commands.start,directions.up);
-			} else {
-				paddle.SetDirection(commands.stop,directions.up);
-			}
 			break;
 		case 39: // right arrow
-			if (event.type == 'keydown') {
-				paddle.SetDirection(commands.start,directions.right);
-			} else {
-				paddle.SetDirection(commands.stop,directions.right);
-			}
 			break;
 		case 40: // down arrow
-			if (event.type == 'keydown') {
-				paddle.SetDirection(commands.start,directions.down);
-			} else {
-				paddle.SetDirection(commands.stop,directions.down);
-			}
 			break;
 		case 49: // 1
 			break;
@@ -63,10 +61,8 @@ function inputHandler(event,paddle,ball) {
 				game.IncreaseSnow();
 			}
 			break;
-		case 80: // P
-			if (event.type == 'keydown') {
-				game.Pause();
-			}
+		case 112: // p - lowercase
+			game.Pause();
 			break;
 		case 83: // S
 			if (event.type == 'keydown') {
@@ -79,6 +75,12 @@ function inputHandler(event,paddle,ball) {
 		default:
 			console.log(event.keyCode);
 			break;
+	}
+}
+
+function MouseHandler(event,paddle,playfield) {
+	if (game.paused == false) {
+		paddle.SetDirection(event.pageX);
 	}
 }
 
@@ -95,6 +97,7 @@ function Game() {
 	this.snowLoop = undefined;
 	this.ballLoop = undefined;
 	this.interval = undefined;
+	this.splashTimer = undefined;
 	this.paused = false;
 	this.score = 0;
 	this.scores = [];
@@ -105,6 +108,7 @@ function Game() {
 	}
 	
 	this.Initialize = function() {
+		console.log('Begin Initialization');
 		_canvas = document.getElementById('playfield');
 		if (_canvas && _canvas.getContext){
 			canvas = _canvas.getContext('2d');
@@ -120,18 +124,17 @@ function Game() {
 		}
 		
 		// Create event handler
-		$(document).keydown(function(e) {
+		$(document).keypress(function(e) {
 			e.preventDefault();
-			inputHandler(e,paddle);
-		});
-		$(document).keyup(function(e) {
-			e.preventDefault();
-			inputHandler(e,paddle);
+			inputHandler(e);
 		});
 		
 		playfield = new Playfield();
+		playfield.LoadImage('images/map.jpg');
 		scoreboard = new Scoreboard();
+		scoreboard.LoadImage('images/scorepanel.jpg');
 		paddle = new Paddle();
+		paddle.LoadImage('images/paddle.png');
 		this.MakeBalls();
 		
 		this.interval = setInterval(self.InitialUpdateRun,this.checkInterval);
@@ -140,15 +143,24 @@ function Game() {
 	this.InitialUpdateRun = function() {
 		if (playfield.Loaded() && self.balls[0].Loaded() && paddle.Loaded() && scoreboard.Loaded()) {
 			clearInterval(self.interval);
+			$(document).mousemove(function(e) {
+				e.preventDefault();
+				MouseHandler(e,paddle,playfield);
+			});
 			
 			// Start game loop
-			self.gameLoop = setInterval(self.Loop,30);
+			self.Loop();
 			self.snowLoop = setInterval(self.SnowLoop,500);
 			self.ballLoop = setInterval(self.BallLoop,5000);
 		}
 	}
 	
+	this.ShowSplash = function(image,context) {
+		context.drawImage(image,0,100);
+	}
+	
 	this.Loop = function() {
+		requestAnimFrame(self.Loop);
 		self.Update();
 		self.Draw();
 	}
@@ -175,9 +187,10 @@ function Game() {
 				self.MoveScores();
 			}
 			
-			//ball.DetectCollision(scoreboard);
 			self.MoveBalls(paddle);
-			paddle.Move();
+			if (!paddle.DetectCollision(playfield)) {
+				paddle.Move();
+			}
 		}
 	}
 	
@@ -189,6 +202,7 @@ function Game() {
 		playfield.Draw(buffer);
 		scoreboard.Draw(buffer);
 		this.DrawBalls(this.balls,buffer);
+		
 		if (this.scores) {
 			this.DrawScores(this.scores,buffer);
 		}
@@ -200,15 +214,23 @@ function Game() {
 		
 		// Draw buffer to canvas
 		canvas.drawImage(_buffer, 0, 0);
+		
+		// FPS measurement
+		if (measureFPS == true) {
+			var thisFrameFPS = 1000 / ((now=new Date) - lastUpdate);
+			fps += (thisFrameFPS - fps) / fpsFilter;
+			lastUpdate = now;
+		}
 	}
 	
 	this.LoadMenu = function() {
 		console.log('Menu Loaded');
 	}
 	
-	this.MakeBalls = function() {
-		var ball = new Ball();
-		this.balls.push(ball);
+	this.MakeBalls = function() {		
+		var tempBall = new Ball();
+		tempBall.LoadImage('images/ball.png');
+		this.balls.push(tempBall);
 	}
 	
 	this.MoveBalls = function(paddle) {
@@ -225,19 +247,37 @@ function Game() {
 		}
 	}
 	
+	this.DrawBalls = function(balls,context) {
+		for(i=0;i<balls.length;i++) {
+			if (balls[i].Loaded) {
+				balls[i].Draw(context);
+			}
+		}
+	}
+	
 	this.MakeSnow = function() {
+		var self = this;
 		for(i=0;i<snowDensity.value;i++) {
 			var flake = new Snowflake();
-			flake.Load();
+			flake.LoadImage();
 			this.snow.push(flake);
 		}
 	}
 	
 	this.MoveSnow = function() {
+		console.log('test');
 		for(i=0;i<this.snow.length;i++) {
 			this.snow[i].Move();
 			if (this.snow[i].y > fieldHeight) {
 				this.snow.splice(i,1);
+			}
+		}
+	}
+	
+	this.DrawSnow = function(snowflakes,context) {
+		for(i=0;i<snowflakes.length;i++) {
+			if (snowflakes[i].Loaded) {
+				snowflakes[i].Draw(context);
 			}
 		}
 	}
@@ -249,18 +289,6 @@ function Game() {
 				this.UpdateScore(this.scores[i].value);
 				this.scores.splice(i,1);
 			}
-		}
-	}
-	
-	this.DrawSnow = function(snowflakes,context) {
-		for(i=0;i<snowflakes.length;i++) {
-			snowflakes[i].Draw(context);
-		}
-	}
-	
-	this.DrawBalls = function(balls,context) {
-		for(i=0;i<balls.length;i++) {
-			balls[i].Draw(context);
 		}
 	}
 	
@@ -321,33 +349,248 @@ function Game() {
 }
 
 function Ball() {
-	var self = this;
-	var image = new Image();
-	image.src = '';
-	image.src = 'images/ball.png';
-	this.x = 0;
 	this.y = -100;
-	this.width = 0;
-	this.height = 0;
 	this.directionY = directions.down;
 	this.speed = 3;
-	this.paused = false;
 	this.value = 100;
-	var _loaded = false;
+	console.log(this);
 	
-	image.onload = function() {
-		self.width = image.width;
-		self.height = image.height;
-		self.x = Math.floor(Math.random() * (fieldWidth - self.width));
-		self.y = Math.floor(Math.random() * -100);
-		_loaded = true;
+	this.LoadImage = function(imgSrc) {
+		var self = this;
+		var image = new Image();
+		image.src = '';
+		image.src = imgSrc;
+		
+		image.onload = function() {
+			self.image = image;
+			self.width = image.width;
+			self.height = image.height;
+			self.x = Math.floor(Math.random() * (fieldWidth - self.width));
+			self.loaded = true;
+		}
+	}
+	
+	this.Move = function() {
+		if (this.paused == false) {
+			this.y += this.speed;
+		}
+	}
+	
+	this.Lost = function() {
+		// Call this when ball goes out of bounds
+	}
+}
+
+function Paddle() {
+	this.x = 20;
+	this.xTarget = 50;
+	this.y = 580;
+	this.direction = directions.left;
+	this.speed = 5;
+	console.log(this);
+	
+	this.GetCenter = function() {
+		var response = this.x + ((this.width + this.speed) / 2);
+		return response;
+	}
+	
+	this.SetDirection = function(targetX) {
+		if (this.GetCenter() < targetX) {
+			this.direction = directions.right;
+		} else if (this.GetCenter() > targetX) {
+			this.direction = directions.left;
+		}
+		this.xTarget = targetX;
+	}
+	
+	this.DetectCollision = function(target) {
+		if (this.x - this.speed < 0 && this.direction == directions.left) {
+			if (debug) {
+				console.log('Paddle Collision - Left: x=' + this.x);
+			}
+			this.x = 0;
+			return 1;
+		}
+		if (this.x + this.width + this.speed > target.width && this.direction == directions.right) {
+			if (debug) {
+				console.log('Paddle Collision - Right: x=' + this.x);
+			}
+			this.x = target.width - this.width;
+			return 1;
+		}
+		return 0;
+	}
+	
+	this.Move = function() {
+		if (this.paused == false) {
+			if (this.direction == directions.left) {
+				if (this.GetCenter() - this.speed < this.xTarget) {
+					// Partial Movement
+					this.direction = undefined;
+				} else {
+					// Full movement space
+					this.x -= this.speed;
+				}
+			}
+			if (this.direction == directions.right) {
+				if (this.GetCenter() + this.speed > this.xTarget) {
+					// Partial Movement
+					this.direction = undefined;
+				} else {
+					// Full movement space
+					this.x += this.speed;
+				}
+			}
+		}
+	}
+	
+	this.Pause = function() {
+		if (paused == false) {
+			// stop paddle movement
+			this.xTarget = this.x;
+			paused = true;
+		} else {
+			paused = false;
+		}
+	}
+}
+
+function Snowflake() {
+	this.xOrigin = 0;
+	this.xDirection = directions.left;
+	this.y = -100; // spawn off the playfield so it won't be visible
+	this.size = Math.floor(Math.random() * (Object.keys(flakeSizes).length));
+	this.xRange = Math.floor(Math.random() * 50);
+	this.speed = Math.floor(Math.random() * 3);
+	console.log(this);
+	
+	this.PickDirection = function() {
+		return directions.left;
+	}
+	
+	this.LoadImage = function() {
+		var self = this;
+		var image = new Image();
+		image.src = '';
+		if (self.size == 0) {
+			image.src = 'images/snowflakeSmall.png';
+		} else if (self.size == 1) {
+			image.src = 'images/snowflakeMedium.png';
+		} else if (self.size == 2) {
+			image.src = 'images/snowflakeLarge.png';
+		}
+		
+		image.onload = function() {
+			self.image = image;
+			self.width = image.width;
+			self.height = image.height;
+			self.x = Math.floor(Math.random() * (fieldWidth - self.width));
+			if (self.x + self.xRange > fieldWidth) { self.xRange = fieldWidth - self.x; }
+			if (self.speed < 1) { self.speed = 1; }
+			self.y = Math.floor(Math.random() * -100);
+			self.xOrigin = self.x;
+			self.xDirection = self.PickDirection();
+			self.loaded = true;
+		}
+	}
+	
+	this.Move = function() {
+		if (this.xDirection == directions.left) {
+			if (this.x > this.xOrigin - this.xRange) {
+				this.x -= 1;
+			} else {
+				this.xDirection = directions.right;
+			}
+		}
+		if (this.xDirection == directions.right) {
+			if (this.x < this.xOrigin + this.xRange) {
+				this.x += 1;
+			} else {
+				this.xDirection = directions.left;
+			}
+		}
+		this.y += this.speed;
+	}
+}
+
+function Playfield() {
+	console.log(this);
+	
+	this.LoadImage = function(imgSrc) {
+		var self = this;
+		var image = new Image();
+		image.src = '';
+		image.src = imgSrc;
+		
+		image.onload = function() {
+			self.image = image;
+			fieldWidth = image.width;
+			fieldHeight = image.height;
+			self.width = image.width;
+			self.height = image.height;
+			self.loaded = true;
+		}
+	}
+}
+
+function Scoreboard() {
+	this.x = 480;
+	console.log(this);
+}
+
+function Score(x,y) {
+	this.value = 100;
+	this.x = x;
+	this.y = y;
+	this.minY = y - 50;
+	this.speed = 5;
+	
+	this.Move = function() {
+		this.y -= this.speed;
+	}
+	
+	this.Draw = function(context) {
+		context.fillText(this.value,this.x,this.y);
+	}
+}
+
+function Drawable() {
+	this.x = 0;
+	this.y = 0;
+	this.width = 0;
+	this.height = 0;
+	this.loaded = false;
+	this.paused = false;
+	this.image = undefined;
+	
+	this.LoadImage = function(imgSrc) {
+		var self = this;
+		var image = new Image();
+		image.src = '';
+		image.src = imgSrc;
+		
+		image.onload = function() {
+			self.image = image;
+			self.width = image.width;
+			self.height = image.height;
+			self.loaded = true;
+		}
 	}
 	
 	this.Loaded = function() {
-		return _loaded;
+		return this.loaded;
 	}
 	
-	this.DetectCollision = function(target,callback) {
+	this.Pause = function() {
+		// stop the ball
+		if (this.paused == true) {
+			this.paused = false;
+		} else {
+			this.paused = true;
+		}
+	}
+	
+	this.DetectCollision = function(target) {
 		if (!((this.x + this.width) >= target.x)) {
 			return 0;
 		}
@@ -363,224 +606,18 @@ function Ball() {
 		return 1;
 	}
 	
-	this.Move = function() {
-		if (this.paused == false) {
-			this.y += this.speed;
-		}
-	}
-	
 	this.Draw = function(context) {
-		context.drawImage(image,this.x,this.y);
-	}
-	
-	this.Pause = function() {
-		// stop the ball
-		if (this.paused == true) {
-			this.paused = false;
-		} else {
-			this.paused = true;
-		}
-	}
-	
-	this.Lost = function() {
-		// Call this when ball goes out of bounds
+		context.drawImage(this.image,this.x,this.y);
 	}
 }
 
-function Paddle() {
-	var self = this;
-	var image = new Image();
-	image.src = '';
-	image.src = 'images/paddle.png';
-	this.x = 20;
-	this.y = 580;
-	this.width = 0;
-	this.height = 0;
-	this.movingLeft = false;
-	this.movingRight = false;
-	this.direction = undefined;
-	this.speed = 3;
-	var paused = false;
-	var _loaded = false;
-	
-	image.onload = function(event) {
-		self.width = image.width;
-		self.height = image.height;
-		_loaded = true;
-	}
-	
-	this.Loaded = function() {
-		return _loaded;
-	}
-	
-	this.Draw = function(context) {
-		context.drawImage(image,this.x,this.y);
-	}
-	
-	this.SetDirection = function(command,direction) {
-		if (paused == false) {
-			if (command == commands.start) {
-				if (direction == directions.left) {
-					this.movingLeft = true;
-				}
-				if (direction == directions.right) {
-					this.movingRight = true;
-				}
-			}
-			if (command == commands.stop) {
-				if (direction == directions.left) {
-					this.movingLeft = false;
-				}
-				if (direction == directions.right) {
-					this.movingRight = false;
-				}
-			}
-		}
-	}
-	
-	this.DetectCollision = function() {
-		
-	}
-	
-	this.Move = function() {
-		if (this.movingLeft == true || this.movingRight == true) {
-			if (this.movingLeft == true) {
-				if (this.x - this.speed >= 0) {
-					this.x -= this.speed;
-				} else {
-					this.x = 0;
-				}
-			}
-			if (this.movingRight == true) {
-				if (this.x + image.width + this.speed <= fieldWidth - this.speed) {
-					this.x += this.speed;
-				} else {
-					this.x = fieldWidth - image.width;
-				}
-			}
-		}
-	}
-	
-	this.Pause = function() {
-		if (paused == false) {
-			// stop paddle and then pause
-			this.movingLeft = false;
-			this.movingRight = false;
-			paused = true;
-		} else {
-			paused = false;
-		}
-	}
-}
-
-function Snowflake() {
-	var self = this;
-	var image = new Image();
-	image.src = '';
-	this.x = 0;
-	this.y = -100; // spawn off the playfield so it won't be visible
-	this.size = Math.floor(Math.random() * 3);
-	this.speed = 3;
-	this.width = 0;
-	var paused = false;
-	var collidable = false;
-	var _loaded = false;
-	
-	this.Load = function() {
-		if (this.size == 0) {
-			image.src = 'images/snowflakeSmall.png';
-		} else if (this.size == 1) {
-			image.src = 'images/snowflakeMedium.png';
-		} else {
-			image.src = 'images/snowflakeLarge.png';
-		}
-	}
-	
-	image.onload = function() {
-		self.width = image.width;
-		self.x = Math.floor(Math.random() * (fieldWidth - self.width));
-		self.y = Math.floor(Math.random() * -100);
-		_loaded = true;
-	}
-	
-	this.Loaded = function() {
-		return _loaded;
-	}
-	
-	this.Draw = function(context) {
-		context.drawImage(image,this.x,this.y);
-	}
-	
-	this.Move = function() {
-		if (paused == false) {
-			this.y += this.speed;
-		}
-	}
-	
-	this.Pause = function() {
-		if (paused == false) {
-			paused = true;
-		} else {
-			paused = false;
-		}
-	}
-}
-
-function Playfield() {
-	var image = new Image();
-	image.src = '';
-	image.src = 'images/map.jpg';
-	var _loaded = false;
-	
-	this.Loaded = function() {
-		return _loaded;
-	}
-	
-	image.onload = function() {
-		_loaded = true;
-		fieldWidth = image.width;
-		fieldHeight = image.height;
-	}
-	
-	this.Draw = function(context) {
-		context.drawImage(image,0,0);
-	}
-}
-
-function Scoreboard() {
-	var image = new Image();
-	image.src = '';
-	image.src = 'images/scorepanel.jpg';
-	this.x = 480;
-	this.y = 0;
-	var collidable = true;
-	var _loaded = false;
-	
-	image.onload = function() {
-		_loaded = true;
-	}
-	
-	this.Loaded = function() {
-		return _loaded;
-	}
-	
-	this.Draw = function(context) {
-		context.drawImage(image,this.x,this.y);
-	}
-}
-
-function Score(x,y) {
-	this.value = 100;
-	this.x = x;
-	this.y = y;
-	this.minY = y - 100;
-	this.speed = 3;
-	
-	this.Move = function() {
-		this.y -= this.speed;
-	}
-	
-	this.Draw = function(context) {
-		context.fillText(this.value,this.x,this.y);
-	}
-}
+Ball.prototype = new Drawable();
+Ball.prototype.constructor = Drawable;
+Snowflake.prototype = new Drawable();
+Snowflake.prototype.constructor = Drawable;
+Playfield.prototype = new Drawable();
+Playfield.prototype.constructor = Drawable;
+Scoreboard.prototype = new Drawable();
+Scoreboard.prototype.constructor = Drawable;
+Paddle.prototype = new Drawable();
+Paddle.prototype.constructor = Drawable;
